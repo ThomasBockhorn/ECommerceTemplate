3597e0105e84638fc036d911631dc49387427001
thomasbockhorn <historian.thomas@gmail.com>
2023-08-06 11:35:33 -0500

    Fixed Styling of ProductSaleTest

--- ECommerceTemplate/tests/Unit/ProductSaleTest.php
+++ ECommerceTemplate/tests/Unit/ProductSaleTest.php
@@ -7,9 +7,6 @@ use PHPUnit\Framework\TestCase;
 
 class ProductSaleTest extends TestCase
 {
-    /**
-     * @return ProductSale
-     */
     protected function getValidProductSale(): ProductSale
     {
         $productSale = new ProductSale();
@@ -20,73 +17,52 @@ class ProductSaleTest extends TestCase
         return $productSale;
     }
 
-    /**
-     * @return void
-     */
     public function test_productsale_exists(): void
     {
         $this->assertTrue(class_exists(ProductSale::class));
     }
 
-    /**
-     * @return void
-     */
-    public function test_to_see_if_productsale_productID_exists()
+    public function test_to_see_if_productsale_productID_exists(): void
     {
         $this->assertContains('ProductID', $this->getValidProductSale()->getFillable());
 
     }
 
-    /**
-     * @return void
-     */
-    public function test_if_productsalequantity_exists()
+    public function test_if_productsalequantity_exists(): void
     {
         $this->assertContains('ProductSaleQuantity', $this->getValidProductSale()->getFillable());
     }
 
-    /**
-     * @return void
-     */
-    public function test_if_productsaleprice_exists()
+    public function test_if_productsaleprice_exists(): void
     {
         $this->assertContains('ProductSalePrice', $this->getValidProductSale()->getFillable());
     }
 
-
-    /**
-     * @return void
-     */
-    public function test_if_productsale_price_is_a_float()
+    public function test_if_productsale_price_is_a_float(): void
     {
         $this->getValidProductSale();
 
         $this->assertIsFloat($this->getValidProductSale()->ProductSalePrice);
     }
 
-    /**
-     * @return void
-     */
-    public function test_if_productsale_price_has_two_decimal_places()
+    public function test_if_productsale_price_has_two_decimal_places(): void
     {
         $this->getValidProductSale();
 
         $this->assertEquals(21.60, $this->getValidProductSale()->ProductSalePrice);
     }
 
-    public function test_producesale_quantity_is_an_integer()
+    public function test_producesale_quantity_is_an_integer(): void
     {
         $this->getValidProductSale();
 
         $this->assertIsInt($this->getValidProductSale()->ProductSaleQuantity);
     }
 
-    public function test_productsale_quantity_is_greater_than_zero()
+    public function test_productsale_quantity_is_greater_than_zero(): void
     {
         $this->getValidProductSale();
 
         $this->assertGreaterThan(-1, $this->getValidProductSale()->ProductSaleQuantity);
     }
-
-
 }
