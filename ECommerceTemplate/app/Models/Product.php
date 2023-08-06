214c50823405e561138f0da66f3cdb4a2cf4722a
thomasbockhorn <historian.thomas@gmail.com>
2023-08-06 00:24:33 -0500

    Refactored test

--- ECommerceTemplate/app/Models/Product.php
+++ ECommerceTemplate/app/Models/Product.php
@@ -5,11 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed|string $ProductName
+ * @property mixed|string $ProductDescription
+ */
 class Product extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'ProductName',
+        'ProductDescription',
     ];
 }
--- ECommerceTemplate/tests/Unit/ProductTest.php
+++ ECommerceTemplate/tests/Unit/ProductTest.php
@@ -3,27 +3,42 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use \App\Models\Product;
 
 class ProductTest extends TestCase
 {
 
-    /**
-     * @return void
-     */
+    protected function getValidProduct(): Product
+    {
+        $product = new Product();
+        $product->ProductName = 'Test Product';
+        $product->ProductDescription = 'Test Product Description';
+        return $product;
+    }
     public function test_model_product_exists(): void
     {
-        $this->assertTrue(class_exists(\App\Models\Product::class));
+        $this->assertTrue(class_exists(Product::class));
     }
 
     /**
      * @return void
      */
-    public function test_property_model_has_product_productname(): void
+    public function test_product_model_has_property_productname(): void
     {
-        $product = new \App\Models\Product();
-
-        $product->ProductName = 'Test Product';
+        $product = $this->getValidProduct();
 
         $this->assertEquals('Test Product', $product->ProductName);
     }
+
+    /**
+     * @return void
+     */
+    public function test_product_model_has_property_productdescription(): void
+    {
+        $product = $this->getValidProduct();
+
+        $this->assertEquals('Test Product Description', $product->ProductDescription);
+    }
+
+
 }
