b6372144b96688e57ba7bb185a1cf40e8f199ec3
thomasbockhorn <historian.thomas@gmail.com>
2023-08-07 17:09:04 -0500

    ProductImage properties are created

--- ECommerceTemplate/tests/Unit/ProductImageTest.php
+++ ECommerceTemplate/tests/Unit/ProductImageTest.php
@@ -7,8 +7,21 @@ use PHPUnit\Framework\TestCase;
 
 class ProductImageTest extends TestCase
 {
+
+    public function getValidProductImage(): ProductImage
+    {
+        return new ProductImage([
+            'ProductID' => 1,
+            'ProductImageURL' => 'https://via.placeholder.com/150',
+        ]);
+    }
     public function test_product_image_exists(): void
     {
         $this->assertTrue(class_exists(ProductImage::class));
+    }
+
+    public function test_to_see_if_product_image_productID_exists(): void
+    {
+        $this->assertContains('ProductID', $this->getValidProductImage()->getFillable());
     }
 }
