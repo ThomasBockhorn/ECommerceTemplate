111b1b542ace5bae5b455b1052ced01935f4d4d8
thomasbockhorn <historian.thomas@gmail.com>
2023-08-09 21:35:46 -0500

    Adding some properties of Invoice

--- ECommerceTemplate/app/Models/Invoice.php
+++ ECommerceTemplate/app/Models/Invoice.php
@@ -5,7 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int|mixed $CustomerID
+ * @property mixed|string $InvoiceDate
+ * @property float|mixed $InvoiceTotal
+ * @property int|mixed $InvoiceNumber
+ */
 class Invoice extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'InvoiceNumber',
+        'InvoiceDate',
+        'InvoiceTotal',
+        'CustomerID',
+    ];
 }
--- ECommerceTemplate/tests/Unit/InvoiceTest.php
+++ ECommerceTemplate/tests/Unit/InvoiceTest.php
@@ -2,14 +2,20 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
 use App\Models\Invoice;
+use PHPUnit\Framework\TestCase;
 
 class InvoiceTest extends TestCase
 {
-    public function test_to_see_if_invoice_model_exists()
+    public function test_to_see_if_invoice_model_exists(): void
     {
         $this->assertTrue(class_exists(Invoice::class));
     }
 
+    public function test_to_see_if_invoice_model_has_property_invoicenumber(): void
+    {
+        $invoice = new Invoice();
+        $invoice->InvoiceNumber = 1;
+        $this->assertEquals(1, $invoice->InvoiceNumber);
+    }
 }
