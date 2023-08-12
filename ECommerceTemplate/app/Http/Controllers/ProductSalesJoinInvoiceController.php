738f8ce8124265c469b00f699bce05a9981c9f73
thomasbockhorn <historian.thomas@gmail.com>
2023-08-12 13:27:55 -0500

    Created a ProductSalesJoinInvoice model

---
+++ ECommerceTemplate/app/Http/Controllers/ProductSalesJoinInvoiceController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProductSalesJoinInvoiceRequest;
+use App\Http\Requests\UpdateProductSalesJoinInvoiceRequest;
+use App\Models\ProductSalesJoinInvoice;
+
+class ProductSalesJoinInvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductSalesJoinInvoiceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductSalesJoinInvoice $productSalesJoinInvoice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProductSalesJoinInvoice $productSalesJoinInvoice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductSalesJoinInvoiceRequest $request, ProductSalesJoinInvoice $productSalesJoinInvoice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductSalesJoinInvoice $productSalesJoinInvoice)
+    {
+        //
+    }
+}
---
+++ ECommerceTemplate/app/Models/ProductSalesJoinInvoice.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductSalesJoinInvoice extends Model
+{
+    use HasFactory;
+}
