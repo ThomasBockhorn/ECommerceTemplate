b07566c7ee44eb6c29e34db284253d602d2065fe
thomasbockhorn <historian.thomas@gmail.com>
2023-08-13 20:33:18 -0500

    fixed a return type

--- ECommerceTemplate/app/Models/ProductSale.php
+++ ECommerceTemplate/app/Models/ProductSale.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * @property int|mixed   $ProductID
@@ -42,7 +43,7 @@ class ProductSale extends Model
         );
     }
 
-    public function product()
+    public function product(): BelongsTo
     {
         return $this->belongsTo(Product::class, 'ProductID', 'id');
     }
