b30554d8880a54e066e4215187cc216242df2a7d
thomasbockhorn <historian.thomas@gmail.com>
2023-08-13 20:50:07 -0500

    customerpayment tests to check if all the model fields exists

--- ECommerceTemplate/tests/Unit/CustomerPaymentTest.php
+++ ECommerceTemplate/tests/Unit/CustomerPaymentTest.php
@@ -31,4 +31,44 @@ class CustomerPaymentTest extends TestCase
 
         $this->assertEquals('Test CustomerPayment', $customerpayment->CustomerPaymentName);
     }
+
+	public function test_to_see_if_CustomerPayment_has_CustomerPaymentCardNumber_property(): void
+	{
+		$customerpayment = $this->getValidCustomerPayment();
+
+		$this->assertEquals('Test CustomerPayment Description', $customerpayment->CustomerPaymentCardNumber);
+	}
+
+	public function test_to_see_if_CustomerPayment_has_CustomerPaymentExpiration_property(): void
+	{
+		$customerpayment = $this->getValidCustomerPayment();
+
+		$this->assertEquals('Test CustomerPayment Description', $customerpayment->CustomerPaymentExpiration);
+	}
+
+	public function test_to_see_if_CustomerPayment_has_CustomerPaymentCVV_property(): void
+	{
+		$customerpayment = $this->getValidCustomerPayment();
+
+		$this->assertEquals('Test CustomerPayment Description', $customerpayment->CustomerPaymentCVV);
+	}
+
+	public function test_to_see_if_CustomerPayment_has_CustomerPaymentDefault_property(): void
+	{
+		$customerpayment = $this->getValidCustomerPayment();
+
+		$this->assertEquals('Test CustomerPayment Description', $customerpayment->CustomerPaymentDefault);
+	}
+
+	public function test_to_see_if_CustomerPayment_has_CustomerID_property(): void
+	{
+		$customerpayment = $this->getValidCustomerPayment();
+
+		$this->assertEquals('Test CustomerPayment Description', $customerpayment->CustomerID);
+	}
+
+	public function test_to_see_if_CustomerPayment_has_CustomerPaymentName_property_in_fillable(): void
+	{
+		$this->assertContains('CustomerPaymentName', $this->getValidCustomerPayment()->getFillable());
+	}
 }
