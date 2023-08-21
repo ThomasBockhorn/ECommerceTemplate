5feaeb843408103b1298cec9f3ed86ca47c22f87
thomasbockhorn <historian.thomas@gmail.com>
2023-08-20 21:36:02 -0500

    created the properties of Admin model

--- ECommerceTemplate/tests/Unit/AdminTest.php
+++ ECommerceTemplate/tests/Unit/AdminTest.php
@@ -11,4 +11,11 @@ class AdminTest extends TestCase
     {
         $this->assertTrue(class_exists(Admin::class));
     }
+
+    public function test_to_see_if_admin_model_has_property_adminname(): void
+    {
+        $admin = new Admin();
+        $admin->AdminName = 'admin';
+        $this->assertEquals('admin', $admin->AdminName);
+    }
 }
